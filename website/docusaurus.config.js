fbbcab9be7558f300e3932855dbd4e73e2e82d55
mkeithX <dev.keith@outlook.com>
2024-01-06 20:30:48 +0800

    post-saurus#000

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
 
 
   // Set the production url of your site here
-  url: 'https://kitiplex.vercel.app',
+  url: 'https://kitiplex.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
