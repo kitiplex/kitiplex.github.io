7fd1a95b3da9cfaee3f4f468297c3580ae687a14
mkeithX <dev.keith@outlook.com>
2024-03-12 04:28:58 +0800

    algo_ap

--- website/src/theme/SearchBar.js
+++ website/src/theme/SearchBar.js
@@ -2,20 +2,23 @@ import React from 'react';
 import { DocSearch } from '@docsearch/react';
 
 import '@docsearch/css';
+
+
 export default function SearchBarWrapper(props) {
   return (
     <div>
       <DocSearch
         appId="KLI8CT6349"
         indexName="kitiplexio.tmp"
-        apiKey="5037344df426897d733d0efebb8b6d12"
+        apiKey="cdf06a43d64035e0552f501bc01ce7e6"
         // placeholder="Search kitiplex..."
 
         resultsFooterComponent={({ state }) => {
           return <h3>{state.context.nbHits} match found</h3>;
         }}
+
         maxResultsPerGroup="7"
-        // insights
+        insights
       />
     </div>
   );
